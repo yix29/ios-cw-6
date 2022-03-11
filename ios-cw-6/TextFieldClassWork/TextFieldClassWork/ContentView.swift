44e151b226e833ace00220360b51926c9108d4a2
حسين علي العجمي <100701775+yix29@users.noreply.github.com>
2022-03-11 19:20:50 +0300

    go

--- ios-cw-6/TextFieldClassWork/TextFieldClassWork/ContentView.swift
+++ ios-cw-6/TextFieldClassWork/TextFieldClassWork/ContentView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     //Add your variables here ضيفوا المتغيرات اهني
+    @State var username : String=""
+    
     
     
     var body: some View {
         VStack(spacing:40){
             //Change the username to the new variable .. غيروا كلمة username الى المتغير الجديد
-            Text("Welcome Back username!")
+            Text("Welcome Back \(username)!")
                 .font(.system(.title, design: .rounded))
                 .fontWeight(.medium)
                 .foregroundColor(Color(red: 0.167, green: 0.152, blue: 0.229))
@@ -23,8 +25,8 @@ struct ContentView: View {
                 .scaledToFit()
             //Add your TextField Here ضيفوه تحت هالسطر
             //.textFieldStyle(.roundedBorder) < عشان تعدلون على شكله تقدرون تضيفون
-            
-            
+            TextField("type username", text: $username)
+                .textFieldStyle(.roundedBorder)
         }.padding()
     }
 }
